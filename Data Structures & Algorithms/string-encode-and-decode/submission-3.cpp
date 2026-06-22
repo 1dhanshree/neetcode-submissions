1e3d34873efefa18bb14c2d78bda7d2fdae63755
Dhanshree <109344596+1dhanshree@users.noreply.github.com>
2026-06-22 13:47:13 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string res;
+        //encode string in the format of string length # and string for neetcode 4#neet4#code
+        for(auto st: strs){
+            res += to_string(st.size()) + '#' + st;
+        }
+        return res;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> result;
+        int i = 0;
+
+        while(i< s.size()){
+            int j = i;
+            //track #
+            while(s[j] != '#'){
+                j++;
+            }
+
+            //get length of the encoded word
+            int len = stoi(s.substr(i, j-i));
+            //move j to word 
+            j++;
+            //get actual word which is encoded
+            string word = s.substr(j, len);
+
+            result.push_back(word);
+
+            i = j+len;
+            
+        }
+        return result;
+    }
+};
